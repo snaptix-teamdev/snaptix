f551a89aba2656395e0565f04d44b42da142643c
Dmitriy Lyashenko <sv-dm@yandex.ru>
2026-05-20 10:33:47 +0300

    feat: add a search page placeholde

---
+++ src/app/search/page.tsx
@@ -0,0 +1,5 @@
+import { Search } from '@/fsd-pages/search/ui/Search'
+
+export default function SearchPage() {
+  return <Search />
+}
---
+++ src/fsd-pages/search/ui/Search.tsx
@@ -0,0 +1,12 @@
+'use client'
+import s from './Search.module.css'
+
+export const Search = () => {
+  return (
+    <section className={s.container}>
+      <div>
+        <h2>Search</h2>
+      </div>
+    </section>
+  )
+}
